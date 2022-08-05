5d33e93687d8984c3992b47a53384c83c270722e
masyoudi <masyoudi@gmail.com>
2022-08-05 16:10:18 +0700

    Feature - Add generate file

---
+++ src/index.ts
@@ -0,0 +1,66 @@
+import bound from "./bound";
+import debounce from "./debounce";
+import gt from "./gt";
+import gte from "./gte";
+import isArray from "./is-array";
+import isBoolean from "./is-boolean";
+import isDate from "./is-date";
+import isEmpty from "./is-empty";
+import isError from "./is-error";
+import isFunction from "./is-function";
+import isNil from "./is-nil";
+import isNull from "./is-null";
+import isNumber from "./is-number";
+import isObjectTag from "./is-object-tag";
+import isObject from "./is-object";
+import isPlainObject from "./is-plain-object";
+import isPrimitive from "./is-primitive";
+import isRegExp from "./is-reg-exp";
+import isString from "./is-string";
+import isSymbol from "./is-symbol";
+import isType from "./is-type";
+import isUndefined from "./is-undefined";
+import lt from "./lt";
+import lte from "./lte";
+import objToString from "./obj-to-string";
+import toChar from "./to-char";
+import toEscape from "./to-escape";
+import toLower from "./to-lower";
+import toUpperFirstWord from "./to-upper-first-word";
+import toUpperFirst from "./to-upper-first";
+import toUpper from "./to-upper";
+import typeOf from "./type-of";
+export {
+  bound,
+  debounce,
+  gt,
+  gte,
+  isArray,
+  isBoolean,
+  isDate,
+  isEmpty,
+  isError,
+  isFunction,
+  isNil,
+  isNull,
+  isNumber,
+  isObjectTag,
+  isObject,
+  isPlainObject,
+  isPrimitive,
+  isRegExp,
+  isString,
+  isSymbol,
+  isType,
+  isUndefined,
+  lt,
+  lte,
+  objToString,
+  toChar,
+  toEscape,
+  toLower,
+  toUpperFirstWord,
+  toUpperFirst,
+  toUpper,
+  typeOf
+}
