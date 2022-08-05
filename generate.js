a53f542801a1a7e94bdc9ec7c3f1a5555649be96
Masyoudi Adi Yanta <masyoudi@gmail.com>
2022-08-05 16:13:22 +0700

    Feature - Add generate file (#1)

---
+++ generate.js
@@ -0,0 +1,32 @@
+import fs from "fs";
+import path from "path";
+
+try {
+  const toCamelCase = (str) => {
+    let arr = str.split("-").map((val, i) => {
+      if (i !== 0) val = val.charAt(0).toUpperCase().concat(val.slice(1));
+      return val;
+    });
+    return arr.join("");
+  };
+
+  let fileNames = fs.readdirSync(path.join(__dirname, "/src"));
+  let filtered = fileNames.filter(
+    (file) => file.split(".").pop() === "ts" && file !== "index.ts"
+  );
+
+  let funcNames = [];
+  let templates = [];
+  filtered.forEach((file) => {
+    file = file.replace(".ts", "");
+    funcNames.push("  ".concat(toCamelCase(file)));
+    templates.push(`import ${toCamelCase(file)} from "./${file}";\n`);
+  });
+  templates.push(`export {\n${funcNames.join(",\n")}\n}\n`);
+
+  let filePath = path.resolve(__dirname, "./src/index.ts");
+  fs.writeFileSync(filePath, templates.join(""));
+  console.info("Index file has been generated");
+} catch {
+  console.warn("Failed to generate index file");
+}
